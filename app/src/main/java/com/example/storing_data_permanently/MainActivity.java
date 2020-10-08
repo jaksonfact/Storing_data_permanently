244b9f3bf6c8b8c49a9839985f093a4762fc58a6
jaksonfact <jaksonfact@gmail.com>
2020-10-08 16:09:59 +0530

    With data saver

---
+++ app/src/main/java/com/example/storing_data_permanently/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.storing_data_permanently;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.zappycode.sharedprefrences", Context.MODE_PRIVATE);
+
+        /*
+        ArrayList<String> friends = new ArrayList<>();
+
+        friends.add("Fido");
+        friends.add("Sarah");
+        friends.add("Jones");
+
+        try {
+            sharedPreferences.edit().putString("friends",ObjectSerializer.serialize(friends)).apply();
+
+            Log.i("friends",ObjectSerializer.serialize(friends));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        */
+
+        ArrayList<String> newFriends = new ArrayList<>();
+
+        try {
+            newFriends = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("friends", ObjectSerializer.serialize(new ArrayList<String>())));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Log.i("new Friends",newFriends.toString());
+
+        //sharedPreferences.edit().putString("username","nick").apply();
+
+        //String username = sharedPreferences.getString("username","");
+
+        //Log.i("This is the username",username);
+
+    }
+}
